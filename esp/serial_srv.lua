2fa8432394c0e0d9e72fa4b842561ad44a7a674a
dromorobo <rbliek@rickor.nl>
2020-02-29 00:02:05 +0100

    Update serial_srv.lua

--- esp/serial_srv.lua
+++ esp/serial_srv.lua
@@ -4,88 +4,97 @@
 -- Setup uart, make permanent (last '1' makes permanent))
 -- (uart,bps,databits,parity,stopbits,echo,permanent = 1)
 
--- uart.setup(0,2400,8,0,1,0,1)
+uart.setup(0,2400,8,0,1,0,1)
 
 -- When '\r' is received, read data and process
+
+VERSION = "0.01"
+EOT = 0x04         -- EOT = End of Transmission
+
+function trim(s)
+  -- trim all heading non-alphanumeric characters
+  pt_start = string.find(s, "%w")
+  if pt_start ~= nil
+  then
+    s = string.sub(s, pt_start)
+  end
+            
+  -- trim all trailing control characters
+  pt_start = string.find(s, "%c")
+  if pt_start ~= nil
+  then
+    s = string.sub(s, 1, pt_start-1)
+  end
+            
+  -- trim all trailing spaces characters
+  -- assuming all heading spaces have 
+  -- already been removed
+  pt_start = string.find(s, "%s")
+  if pt_start ~= nil
+  then
+    s = string.sub(s, 1, pt_start-1)
+  end
+  return s
+end
+
 uart.on("data", "\r",
   function(data)
-    if (string.find(data, "getip") ~= nil)
+    if (string.find(data, "get ") ~= nil)
     then
-      ip, nm = wifi.sta.getip()
-      if ip ~= nil
+      what = string.sub(data, 5)
+      trim(what)
+      if (string.find(what, "version") ~= nil)
       then
-        uart.write(0, ip)
-        uart.write(0, 0x04)  -- EOT = End of Transmission
-      else
-        uart.write(0, "nil")
-        uart.write(0, 0x04)  -- EOT = End of Transmission
-      end -- ip ~= nil
+        uart.write(0, version)
+        uart.write(0, EOT)
+        
+      elseif (string.find(data, "ip") ~= nil)
+      then
+        ip, nm = wifi.sta.getip()
+        if ip ~= nil
+        then
+          uart.write(0, ip)
+          uart.write(0, EOT) 
+        else
+          uart.write(0, "nil")
+          uart.write(0, EOT)
+        end
     
-    elseif (string.find(data, "getmask") ~= nil)
+      elseif (string.find(data, "netmask") ~= nil)
       then
         ip, nm = wifi.sta.getip()
         if nm ~= nil
         then
           uart.write(0, nm)
-          uart.write(0, 0x04)  -- EOT = End of Transmission
+          uart.write(0, EOT)
         else
           uart.write(0, "nil")
-          uart.write(0, 0x04)  -- EOT = End of Transmission
+          uart.write(0, EOT)
         end -- nm ~= nil
+      end
 
     elseif (string.find(data, "whois ") ~= nil)
-      then -- find the "who"
-        len = string.len(data)
-        pt_start, pt_end = string.find(data,"whois ")
-        if pt_start < len
-        then
-          -- remove "whois " and trim
-          who = string.sub(data,pt_end+1)
-          
-          if (who ~= nil)
-          then
+    then 
+      fqdn = string.sub(data, 7)
+      fqdn = trim(fqdn)
+      if (fqdn ~= nil)
+      then
+        net.dns.resolve(fqdn, function(sk, ip)
+          if (ip ~= nil) 
+          then uart.write(0,ip)
+          else uart.write(0,"host not found")
+          end
+          uart.write(0, EOT)
+        end)
+      end
 
-            -- trim all heading non-alphanumeric characters
-            pt_start = string.find(who, "%w")
-            if pt_start ~= nil
-            then
-              who = string.sub(who, pt_start)
-            end
-            
-            -- trim all trailing control characters
-            pt_start = string.find(who, "%c")
-            if pt_end ~= nil
-            then
-              who = string.sub(who, 1, pt_start-1)
-            end
-            
-            -- trim all trailing spaces characters
-            -- assuming all heading spaces have 
-            -- already been removed
-            pt_start = string.find(who, "%s")
-            if pt_start ~= nil
-            then
-              who = string.sub(who, 1, pt_start-1)
-            end
-
-            net.dns.resolve(who, function(sk, ip)
-              if (ip ~= nil) 
-              then uart.write(0,ip)
-              else uart.write(0,"host not found")
-              end
-              uart.write(0, 0x04)  -- EOT = End of Transmission
-            end)
-                      
-          else
-            uart.write(0, "wrong parameter")
-            uart.write(0, 0x04)  -- EOT = End of Transmission
-          end -- (who ~= nil)
-        else
-          uart.write(0, "parameter missing")
-          uart.write(0, 0x04)  -- EOT = End of Transmission
-        end -- len>6
+    elseif (string.find(data, "version") ~= nil)
+    then 
+      uart.write(0, VERSION)
+      uart.write(0, EOT)
+                            
     else 
       uart.write(0, "Incorrect Command")
-      uart.write(0, 0x04)  -- EOT = End of Transmission
+      uart.write(0, EOT)
     end  
   end, 0)
