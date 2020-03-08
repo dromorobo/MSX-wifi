91e715f4dcf19d1eb78d93d2dd6c28b5e2e9ba7a
dromorobo <rbliek@rickor.nl>
2020-03-08 16:58:54 +0100

    Update serial_srv.lua

--- esp/serial_srv.lua
+++ esp/serial_srv.lua
@@ -8,14 +8,14 @@
 
 -- When '\r' is received, read data and process
 
-local VERSION = "0.01a"
+local VERSION = "0.02"
 local EOT = 0x04         -- EOT = End of Transmission
-
-local http_buffer = nil
+local what = ""
+local buffer = nil
 
 function trim(s)
-  -- trim all heading non-alphanumeric characters
-  pt_start = string.find(s, "%w")
+  -- trim all heading spaces characters
+  pt_start = string.find(s, "^%s")
   if pt_start ~= nil
   then
     s = string.sub(s, pt_start)
@@ -45,12 +45,7 @@ uart.on("data", "\r",
     then
       what = string.sub(data, 5)
       trim(what)
-      if (string.find(what, "version") ~= nil)
-      then
-        uart.write(0, VERSION)
-        uart.write(0, EOT)
-        
-      elseif (string.find(data, "ip") ~= nil)
+      if (string.find(what, "ip") ~= nil)
       then
         ip, nm = wifi.sta.getip()
         if ip ~= nil
@@ -77,40 +72,127 @@ uart.on("data", "\r",
 
     elseif (string.find(data, "whois ") ~= nil)
     then 
-      fqdn = string.sub(data, 7)
-      fqdn = trim(fqdn)
-      if (fqdn ~= nil)
+      what = string.sub(data, 7)
+      what = trim(what)
+      if (what ~= nil)
       then
-        net.dns.resolve(fqdn, function(sk, ip)
+        net.dns.resolve(what, function(sk, ip)
           if (ip ~= nil) 
-          then 
-            uart.write(0,ip)
-          else 
-            uart.write(0,"host not found")
+          then uart.write(0,ip)
+          else uart.write(0,"host not found")
           end
-          uart.write(0, EOT)
         end)
       end
+      uart.write(0, EOT)
 
-    elseif (string.find(data, "http ") ~= nil)
-    then 
-      local uri = string.sub(data, 6)
-      local code = nil
-
-      uri = trim(uri)
-      if (uri ~= nil)
+    elseif (string.find(data, "show ") ~= nil)
+    then
+      what = string.sub(data, 6)
+      what = trim(what)
+      if (what ~= nil)
       then
-        http.get("http://" .. uri, nil, function(code, data)
-          if (code < 0) then
-            print("HTTP request failed")
+        if (string.find(data, "version") ~= nil)
+        then 
+          uart.write(0, VERSION)
+
+        elseif (string.find(data, "buflength") ~= nil)
+        then
+          if (buffer ~= nil)
+          then
+            uart.write(0, tostring(string.len(buffer)))
           else
-            uart.write(0,data)
-            uart.write(0, EOT)
+            uart.write(0, "0")
           end
-        end)
-      end                            
+        else
+          uart.write(0, "Syntax Error")
+        end
+      else
+        uart.write(0, "Nothing to show")
+      end
+      uart.write(0, EOT)
+  
+    elseif (string.find(data, "readchar") ~= nil)
+    then
+      if (buffer ~= nil)
+      then
+        uart.write(0, string.sub(buffer,1,1))
+        if (string.len(buffer)>1)
+        then
+          buffer = string.sub(buffer,2)
+        else
+          buffer = nil
+        end
+      else
+        uart.write(0, "Nothing to read")
+      end
+      uart.write(0, EOT)
+
+    elseif (string.find(data, "readbuffer") ~= nil)
+    then
+      if (buffer ~= nil)
+      then
+        uart.write(0, buffer)
+        buffer = nil
+      else
+        uart.write(0, "Nothing to read")
+      end
+      uart.write(0, EOT)
+
+    elseif (string.find(data, "clearbuffer") ~= nil)
+    then
+      buffer = nil
+      uart.write(0, EOT)
+
+    elseif (string.find(data, "open ") ~= nil)
+    then 
+      sloc = string.find(data, " ")
+      what = trim(string.sub(data, sloc+1))
+      if (string.find(what, "http://") ~= nil)
+      then
+        what = string.sub(what, 8)
+        sloc = string.find(what, "/")
+        if (sloc ~= nil)
+        then
+          host = string.sub(what, 1, sloc-1)
+          item = string.sub(what,sloc)
+          item = trim(item)
+          print(item)
+        else
+          host = what
+          item = "/"
+        end
+
+        host = trim(host)
+        print(host)
+
+        if (host ~= nil)
+        then
+          -- open connection
+          conn = net.createConnection(net.TCP, 0)
+
+          conn:on("receive", function(sck, c)
+            if buffer == nil 
+            then
+              buffer = c
+            else
+              buffer = buffer .. c
+            end
+          end)
+
+          -- In case of connection send request
+          conn:on("connection", function(sck, c)
+            request = "GET " .. item .. " HTTP/1.1\r\nHost: " .. host .. "\r\n\r\n"
+            print(request)
+            conn:send(request)
+          end)
+
+          -- Start connection
+          conn:connect(80,host)
+        end    
+      end        
+                  
     else 
-      uart.write(0, "Incorrect Command")
+      uart.write(0, "Error 21")
       uart.write(0, EOT)
     end  
   end, 0)
