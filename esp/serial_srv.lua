c1aa4c6faafa5fa71014f2e345644b8e20bdf94e
dromorobo <rbliek@rickor.nl>
2020-03-12 15:14:52 +0100

    Added help command and help file

--- esp/serial_srv.lua
+++ esp/serial_srv.lua
@@ -4,11 +4,10 @@
 -- Setup uart, make permanent (last '1' makes permanent))
 -- (uart,bps,databits,parity,stopbits,echo,permanent = 1)
 
-uart.setup(0,2400,8,0,1,0,1)
-
 -- When '\r' is received, read data and process
 
-local VERSION = "0.02"
+local VERSION = "0.03"
+local BPS = 9600         -- Speed of serial interface
 local EOT = 0x04         -- EOT = End of Transmission
 local what = ""
 local buffer = nil
@@ -39,6 +38,22 @@ function trim(s)
   return s
 end
 
+-- Let's see if there is a config file for serial
+if file.exists("serial.cfg")
+then
+  file.open("serial.cfg")
+  line = trim(file.readline())
+  BPS = tonumber(line)
+  file.close()
+end
+
+if (BPS~=1200 and BPS~=2400 and BPS~=9600 and BPS~=19200) 
+then
+  BPS=9600
+end
+
+uart.setup(0,BPS,8,0,1,0,1)
+
 uart.on("data", "\r",
   function(data)
     if (string.find(data, "get ") ~= nil)
@@ -51,11 +66,10 @@ uart.on("data", "\r",
         if ip ~= nil
         then
           uart.write(0, ip)
-          uart.write(0, EOT) 
         else
           uart.write(0, "nil")
-          uart.write(0, EOT)
         end
+        uart.write(0, EOT) 
     
       elseif (string.find(data, "netmask") ~= nil)
       then
@@ -63,12 +77,39 @@ uart.on("data", "\r",
         if nm ~= nil
         then
           uart.write(0, nm)
-          uart.write(0, EOT)
         else
           uart.write(0, "nil")
-          uart.write(0, EOT)
-        end -- nm ~= nil
+        end
+        uart.write(0, EOT)
+
+      elseif (string.find(data, "speed") ~= nil)
+      then
+        -- Let's see if there is a config file for serial
+        if file.exists("serial.cfg")
+        then
+          file.open("serial.cfg")
+          line = trim(file.readline())
+          uart.write(0, "Startup = " .. line .. "\n\r")
+          file.close()
+        end
+        uart.write(0, "Running = " .. BPS .. "\n\r")
+        uart.write(0, EOT)    
       end
+
+    elseif (string.find(data, "set ") ~= nil)
+    then
+      what = string.sub(data, 5)
+      if (string.find(what, "speed ") ~= nil)
+      then
+        speed = string.sub(what, 7)
+        if speed ~= nil
+        then -- Write the result in seial.cfg
+          file.open("serial.cfg","w+")
+          file.write(tostring(speed) .. "\n")
+          file.close()
+        end
+      end
+      uart.write(0, EOT)
 
     elseif (string.find(data, "whois ") ~= nil)
     then 
@@ -85,6 +126,11 @@ uart.on("data", "\r",
       end
       uart.write(0, EOT)
 
+    elseif (string.find(data, "restart") ~= nil)
+    then 
+      uart.write(0, EOT)
+      node.restart()
+    
     elseif (string.find(data, "show ") ~= nil)
     then
       what = string.sub(data, 6)
@@ -189,7 +235,23 @@ uart.on("data", "\r",
         end    
       end
       uart.write(0, EOT)
-                  
+    
+    elseif (string.find(data, "help") ~= nil)
+    then
+      if file.exists("help.hlp")
+      then
+        file.open("help.hlp")
+        line = file.readline()
+        while (line ~= nil)
+        do
+          uart.write(0, line)
+          uart.write(0, "\r")    -- compensate for LF to LFCR
+          line = file.readline()
+        end
+        file.close()
+      end 
+      uart.write(0, EOT)
+              
     else 
       uart.write(0, "Error 21")
       uart.write(0, EOT)
